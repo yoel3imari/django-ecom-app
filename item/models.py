9d65b10088bfca2a6af0f83ed401780fd3b18d9c
yoel3imari <youssef.elaimari@gmail.com>
2024-12-21 15:01:47 +0100

    init

---
+++ item/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_sold = models.BooleanField(default=False)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='items'
+    )
+    created_by = models.ForeignKey(
+        User,
+        related_name='items',
+        on_delete = models.CASCADE
+    )
+    image = models.ImageField(upload_to='item_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Items'
+
+    def __str__(self):
+        return self.name
+
---
+++ item/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from item.models import Item, Category
+# Create your views here.
+
+def index(request):
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    return render(request, 'core/index.html', {'items': items, 'categories': categories})
